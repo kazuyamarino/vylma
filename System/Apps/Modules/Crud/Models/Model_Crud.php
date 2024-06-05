34e7cd0ac9b63d40b4840c8bf86a00c8de67982e
kazuyamarino <vikry.yuansah@gmail.com>
2024-06-05 16:48:13 +0700

    create migration

--- System/Apps/Modules/Crud/Models/Model_Crud.php
+++ System/Apps/Modules/Crud/Models/Model_Crud.php
@@ -10,7 +10,7 @@ class Model_Crud extends DB
 	public function get_data()
 	{
 		// query to display the data table in json form
-		$query = "SELECT * FROM tb_users";
+		$query = "SELECT * FROM crud_table";
 		$data = DB::connect()->query($query)->style(FETCH_ASSOC)->fetch_all();
 		$json_data = json_fetch([
 			"data" => $data
@@ -21,16 +21,16 @@ class Model_Crud extends DB
 
 	public function get_user_code()
 	{
-		// get last id from tb_users
-		$q_select_id = "SELECT MAX(id) FROM tb_users";
+		// get last id from crud_table
+		$q_select_id = "SELECT MAX(id) FROM crud_table";
 		$last_id = DB::connect()->query($q_select_id)->fetch_column();
 		terner(not_filled($last_id), $last_id = 0, $last_id); // if last id is 0 then give it 0 value
 
-		// get last user_code second number from tb_users
+		// get last user_code second number from crud_table
 		$q_select_ucode = "SELECT
 			id as id,
 			SUBSTR(user_code, 2, 1) as ucode
-		FROM tb_users ORDER BY id DESC LIMIT 1";
+		FROM crud_table ORDER BY id DESC LIMIT 1";
 		$get_ucode_num = DB::connect()->query($q_select_ucode)->style(FETCH_ASSOC)->fetch();
 		terner($get_ucode_num['ucode'] == 9, ($last_ucode = 0), ($last_ucode = 1 + (int)$get_ucode_num['ucode'])); // if ucode reach 9 then reset it to 0
 
@@ -44,7 +44,7 @@ class Model_Crud extends DB
 	public function insert_data($param)
 	{
 		// begin for insert data
-		$q_insert_user = "INSERT INTO tb_users(
+		$q_insert_user = "INSERT INTO crud_table(
 			user_code,
 			user_name,
 			user_password,
@@ -66,21 +66,21 @@ class Model_Crud extends DB
 	public function delete_data($param)
 	{
 		// query to delete the data table
-		$query = "DELETE FROM tb_users WHERE id = :id";
+		$query = "DELETE FROM crud_table WHERE id = :id";
 		DB::connect()->query($query)->vars($param)->exec();
 	}
 
 	public function multidelete_data($data)
 	{
 		// query to delete more than one data table
-		$query = "DELETE FROM tb_users WHERE id IN ($data[0])";
+		$query = "DELETE FROM crud_table WHERE id IN ($data[0])";
 		DB::connect()->query($query)->vars($data[1])->exec();
 	}
 
 	public function update_data_password_null($param)
 	{
 		// query for data updates without a password
-		$query = "UPDATE tb_users SET
+		$query = "UPDATE crud_table SET
 			user_name     = :user_name,
 			user_password = user_password,
 			user_status   = :user_status,
@@ -94,7 +94,7 @@ class Model_Crud extends DB
 	public function update_data_password_yes($param)
 	{
 		// query for data updates with a password
-		$query = "UPDATE tb_users SET
+		$query = "UPDATE crud_table SET
 			user_name     = :user_name,
 			user_password = :user_password,
 			user_status   = :user_status,
@@ -108,7 +108,7 @@ class Model_Crud extends DB
 	public function fetch_update($param)
 	{
 		// fetch data from the data table for updating purposes
-		$query = "SELECT * FROM tb_users WHERE id = :id";
+		$query = "SELECT * FROM crud_table WHERE id = :id";
 		$data = DB::connect()
 			->query($query)
 			->vars($param)
